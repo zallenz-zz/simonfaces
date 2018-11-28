9895c9cf22a98dece77b36b9a01cd45b7c3e33fe
zallenz <allen.zhou@tufts.edu>
2018-11-28 13:24:25 -0500

    added contract for emotion

---
+++ public/emotion.js
@@ -0,0 +1,14 @@
+/*
+Takes: JSON.stringify(faces[0].emotions, function(key, val) {
+            return val.toFixed ? Number(val.toFixed(0)) : val;
+          }));
+          JSON.stringify(faces[0].expressions, function(key, val) {
+            return val.toFixed ? Number(val.toFixed(0)) : val;
+          }));
+Returns: "Happy", "Sad", "Angry", "Surprise"
+*/
+function guessEmotion()
+{
+
+
+}

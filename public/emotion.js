2c02c060546a43b753a102e55b94f9d17e1d30a3
Ranjit Thomas <ranjit.thomas@tufts.edu>
2018-12-03 01:46:46 -0500

    wrote the function that determine whether a face is angry, sad, suprised, or angry

--- public/emotion.js
+++ public/emotion.js
@@ -9,7 +9,14 @@ Returns: "happy", "sad", "angry", "surprised"
 */
 function guessEmotion(emotions, expressions)
 {
-//	console.log(emotions, expressions);
-	return "happy";
+	// what would the default emotion be if we can't read anything? 
+	if (emotions['joy'] > 80) console.log("happy");
 
+	if (emotions['valence'] < -50) console.log("sad");
+
+	if (emotions['anger'] > 80 || expressions['browFurrow'] > 80) console.log("angry");
+
+	if (expressions['lipPucker'] > 20) console.log("surprised");;
+
+	return "neutral";
 }
